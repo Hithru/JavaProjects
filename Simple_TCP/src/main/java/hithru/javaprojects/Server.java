a906bd67cbd8136b8162db0eac5ed6a4648f7a80
Hithru <hithrudealwis@gmail.com>
2023-02-23 01:15:40 +0530

    Simple TCP

---
+++ Simple_TCP/src/main/java/hithru/javaprojects/Server.java
@@ -0,0 +1,33 @@
+package hithru.javaprojects;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public Server() throws Exception{
+
+        ServerSocket server_socket = new ServerSocket(2020);
+        System.out.println("Port 2020 is open");
+        Socket socket = server_socket.accept();
+        System.out.println("Client "+socket.getInetAddress()+ " has connected");
+
+        BufferedReader in_socket = new BufferedReader(new InputStreamReader((socket.getInputStream())));
+        PrintWriter out_socket = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+
+        out_socket.println("Welcome !");
+        String message = in_socket.readLine();
+        System.out.println("Client says: "+ message);
+
+        socket.close();
+        System.out.println("Socket is closed");
+
+    }
+    public static void main(String[] args){
+        try {
+            new Server();
+        }catch (Exception e){
+
+        }
+    }
+}
